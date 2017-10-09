fee340c567f167813d602fd0fc459dd2357c2175
PliutaMykhailo <mykhailo.pliuta@gmail.com>
2017-10-09 20:14:01 +0300

    Update Main.java

--- src/main/java/com/plyuta/task1/Main.java
+++ src/main/java/com/plyuta/task1/Main.java
@@ -8,17 +8,18 @@ import java.io.IOException;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
-        SayHellow sayHellow = new SayHellow();
+     public static void main(String[] args) throws IOException {
+
+        SayHellow sayHellow;
         if (args.length == 0) {
             System.out.println("There are no arguments.");
         } else if (args.length == 1) {
-            sayHellow.printHello(args[0], sayHellow.getZone(args[0]));
+            sayHellow = new SayHellow(args[0]);
+            sayHellow.printHello();
         } else if (args.length == 2) {
-            sayHellow.printHello(args[0], args[1]);
+            sayHellow = new SayHellow(args[0], args[1]);
+            sayHellow.printHello();
         }
+
     }
 }
